5095f4532ce96e15c55d2165b9c903f8be38e997
VoitMax <maximilian.voit@student.uibk.ac.at>
2022-04-03 14:18:23 +0200

    Beautify all

--- nz/main.js
+++ nz/main.js
@@ -3,10 +3,10 @@
 
 
 var map = L.map("map").setView([-40.921, 172.9609], 11);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-        L.marker([-40.921, 172.9609]).addTo(map)
-            .bindPopup('<h2>Abel-Tasman-Nationalpark</h2>')
-            .openPopup();
+L.marker([-40.921, 172.9609]).addTo(map)
+    .bindPopup('<h2>Abel-Tasman-Nationalpark</h2>')
+    .openPopup();
